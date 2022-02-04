abc72ae4b9610ce145dd0aa843cdd23d6db2e4b6
Sarthak Tripathy <sarthaktripathy1621@gmail.com>
2022-02-04 22:26:00 +0530

    LogIn Partially Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './MyComponent/log-in/log-in.component';
+import { RegistrationComponent } from './MyComponent/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
