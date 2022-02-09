778bccf042f7052fa5992dd0cc4d2525b36e973f
Sarthak Tripathy <sarthaktripathy1621@gmail.com>
2022-02-09 12:47:53 +0530

    Route Module Properly set and component CSS done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LogInComponent } from './MyComponent/log-in/log-in.component';
+import {  RegistrationComponent } from './MyComponent/registration/registration.component';
+import { LogINNavComponent } from './MyComponent/log-innav/log-innav.component';
+
+const routes: Routes = [{
+  path : "register",
+  component : RegistrationComponent
+},
+{
+  path : "login",
+  component :LogInComponent
+},
+{
+  path : "",
+  component : LogINNavComponent
+},
+];
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    
   ]
 })
 export class AppRoutingModule { }
